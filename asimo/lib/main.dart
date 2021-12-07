f347655d9db01285e2f4e69d0363aed6bac6a44c
dogan1972 <95640690+dogan1972@users.noreply.github.com>
2021-12-07 15:56:50 +0300

    Update main.dart

--- asimo/lib/main.dart
+++ asimo/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
               ),
               IconButton(onPressed: () {}, icon: Icon(Icons.apps))
             ],
-            title: Text("Kivroş Asımın Dünyasıdır."),
+            title: Text("Asım baba Asımdır."),
           ),
           body: AsimoTabControllers()),
     );
